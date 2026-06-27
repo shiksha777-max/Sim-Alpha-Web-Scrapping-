3c71554316c289baaa9270621dbe090a2a29d686
shiksha777-max <shikshanw@gmail.com>
2026-06-27 13:53:26 +0545

    Update __all__ to exclude unused writer functions
    Removed fetch_unanalyzed and update_sentiments from __all__.

--- db/__init__.py
+++ db/__init__.py
@@ -1,11 +1,9 @@
 from .connection import ensure_table_exists, get_connection
-from .writer import fetch_unanalyzed, update_sentiments, write_articles, write_market_snapshot
+from .writer import write_articles, write_market_snapshot
 
 __all__ = [
     "ensure_table_exists",
     "get_connection",
-    "fetch_unanalyzed",
-    "update_sentiments",
     "write_articles",
     "write_market_snapshot",
 ]
