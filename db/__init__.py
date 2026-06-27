cd45a11aef990ee0279effb69718d34538fa50c3
shiksha777-max <shikshanw@gmail.com>
2026-06-27 18:06:19 +0545

    Replace write_market_snapshot with write_nepse_index

--- db/__init__.py
+++ db/__init__.py
@@ -1,9 +1,9 @@
 from .connection import ensure_table_exists, get_connection
-from .writer import write_articles, write_market_snapshot
+from .writer import write_articles, write_nepse_index
 
 __all__ = [
     "ensure_table_exists",
     "get_connection",
     "write_articles",
-    "write_market_snapshot",
+    "write_nepse_index",
 ]
