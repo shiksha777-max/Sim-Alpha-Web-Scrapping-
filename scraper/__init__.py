bcebb19f716b5ffbfc1aaf71d8073ed7b00fc4a8
shiksha777-max <shikshanw@gmail.com>
2026-06-27 18:07:56 +0545

    Add scrape_all_nepse_data to __all__ exports

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,4 +1,5 @@
 from .scrapers import scrape_all
 from .scheduler import run_pipeline, start_scheduler
+from .nepse_scraper import scrape_all_nepse_data
 
-__all__ = ["scrape_all", "run_pipeline", "start_scheduler"]
+__all__ = ["scrape_all", "run_pipeline", "start_scheduler", "scrape_all_nepse_data"]
